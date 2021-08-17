0f2b7ff9af204ae3ef392b7017ad0e8d71c7ad60
vankoto2 <it.work.bulgaria@gmail.com>
2021-08-17 22:02:29 +0300

    CalculateLongestTeemWork.js up and working

--- src/helpers/CalculateLongestTeemWork.js
+++ src/helpers/CalculateLongestTeemWork.js
@@ -1,137 +1,97 @@
 const mostTimeWorkedTogether = [];
+const test = [];
 const projects = [];
-const finalProjects = [];
+
 /**
- * calculateLongestTeemWork() returns array of the two workers who worked togeder on a project
- * and have lorked the longest period of time;
- * *calculate teamwork receive number and array;
+ * calculateLongestTeemWork() returns object firstEmplID, secondEmplID, projectID, daysWorkedTogether
+ * *the two workers who worked togeder on a project
  */
 const calculateLongestTeemWork = (array) => {
   const tempArray = [...array];
 
   array.forEach((element) => {
     let acc = [];
-    // console.log(element);
-    // console.log(tempArray[0]);
-
     for (let i = 0; i < tempArray.length; i += 1) {
-      // console.log(`element   ${element.projectID}   ${tempArray[i].projectID}    tempArray`);
-
       if (element.projectID === tempArray[i].projectID) {
-        // console.log(acc);
         acc.push(tempArray[i]);
       }
     }
     projects.push(acc);
   });
 
+  let filteredProjects = projects.sort().filter(removeSingles)
 
-  for (let i = 0; i < projects.length; i += 1) {
-    if (projects[i].length <= 1) {
-      projects.splice(i, 1);
-    }
+
+  /**
+   * removeSingles() returns array of objects
+   * Removes all single projects
+   */
+  function removeSingles(params) {
+    return params.length > 1
   }
 
-let tempProjects = [...projects]
-console.log(projects);
-console.log(tempProjects[0]);
-
-for (let i = 0; i < projects.length; i += 1) {
-  if (projects[i].length <= 1) {
-    projects.splice(i, 1);
-  }  
-}
-  for (let i = 0; i < projects.length; i += 1) {
-
-    finalProjects.push(projects[i]);
-
-    for (let s = 0; s < tempProjects.length; s += 1) {
-      // console.log("fsrst");
-      // console.log(projects[i]);
-      // console.log('second');
-      // console.log( projects[s] );
-      if (tempProjects[s] === projects[i]) {
-        console.log('vliza');
-        console.log(projects);
-        // console.log(projects[i]);
-        projects.splice(i, 1);
-
+  //Removes all duplicates
+  filteredProjects.forEach((c) => {
+    filteredProjects.forEach(element => {
+      if (element === c) {
+        let i = filteredProjects.indexOf(c);
+        filteredProjects.splice(i, 1);
       }
-    }
-  };
-
-
-  console.log('krai');
-  console.log(finalProjects);
-
-  // console.log(projects);
-
-
-  projects.forEach((element) => {
-    //   console.log(element);
-    // console.log( element);
-    if (element.length !== 2) {
-      let first = element[0];
-      let second = element[1];
-
-      for (let i = 0; i < element.length; i += 1) {
-        // if (element[i].daysWorked > first.daysWorked) {
-        //   first = element[i];
-        // }
-      }
-      for (let i = 0; i < element.length; i += 1) {
-        // if (element[i].daysWorked > second.daysWorked || second.daysWorked < first) {
-        //   second = element[i];
-        // }
-      }
-      //   console.log(element)
-      //   console.log(first)
-      //   console.log(second)
-    }
+    });
   });
 
+  //filters the two most worked persons at evry project
+  filteredProjects.forEach((element) => {
+    let first = element[0];
+    let second;
 
+    for (let i = 0; i < element.length; i += 1) {
+      if (element[i].daysWorked > first.daysWorked) {
+        first = element[i];
 
+      }
+    }
 
-  //   for (let i = 1; i < tempArray.length; i += 1) {
-  //     const { emplID } = tempArray[i];
-  //     const previousEmplID = tempArray[i - 1].emplID;
-  //     const { projectID } = tempArray[i];
-  //     const previousprojectID = tempArray[i - 1].projectID;
-  //     const { daysWorked } = tempArray[i];
-  //     const previousdaysWorked = tempArray[i - 1].daysWorked;
+    element.splice(element.indexOf(first), 1);
 
-  //     if (projectID === previousprojectID) {
-  //       projects.push(tempArray[i - 1]);
-  //       console.log('vliza');
-  //     }
+    element.forEach(e => {
+      second = e;
+      if (e.daysWorked > second.daysWorked) {
+        second = e;
+      }
+    });
 
-  // console.log('line     ' + emplID + '    ID   ' + '     previousline    ' +previousEmplID);
-  // console.log('line   ' + projectID + '   proj    ' + '     previousline    ' +previousprojectID);
-  // console.log('line    ' + daysWorked + '    time    ' + '     previousline    ' +previousdaysWorked);
+    for (let i = 0; i < element.length; i += 1) {
+      if (element[i].daysWorked !== undefined) {
+        if (element[i].daysWorked > second.daysWorked) {
+          second = element[i];
+        }
+      }
+    }
+    test.push(first)
+    test.push(second)
+  });
+
+  let tesp = 0
+  let temp = []
+  for (let i = 1; i < test.length; i += 2) {
+    tesp = Number(test[i].daysWorked) + Number(test[i - 1].daysWorked)
+    temp.push([test[i], test[i - 1], tesp])
+
+  }
+  temp.sort(function (b, a) {
+    return a[2] - b[2];
+  });
+
+  let mostTimeWorkedTogether = temp[0];
+
+  const answer = {
+    firstEmplID: mostTimeWorkedTogether[0].emplID,
+    secondEmplID: mostTimeWorkedTogether[1].emplID,
+    projectID: mostTimeWorkedTogether[0].projectID,
+    daysWorkedTogether: mostTimeWorkedTogether[0].daysWorked + mostTimeWorkedTogether[1].daysWorked,
+  }
+
+  console.log(answer);
 }
-
-// console.log(projects);
-// empProjTime.push({
-//     emplID: readyData[0],
-//     projectID: readyData[1],
-//     daysWorked: totalWorkTime,
-//   });
-
-// console.log(tempArray[0].projectID);
-// return(projectArray)
-// };
-
-// const blabla = (array) => {
-//     let temp = []
-
-//     for (let i = 1; i < projectArray.length; i++) {
-//         const element = projectArray[i].projectId;
-//         const element2 = projectArray[i-1].projectId;
-//         if (element === element2) {
-//             sameProjects.push(projectArray[i])
-//         }
-//     }
-// }
-
 export default (calculateLongestTeemWork);
