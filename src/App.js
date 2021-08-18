1c1af88cf83d471028c920cc3a960939ed889b65
vankoto2 <it.work.bulgaria@gmail.com>
2021-08-18 15:09:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import DataGridDemo from './components/output/Output'
 function App() {
   return (
     <div className="App">
-      <h1>Search for the two team members that worked longest on a sigle projects together</h1>
+      <h1>Search for the two team members that worked longest on a single projects together</h1>
       <InputFile add={addFile} />
       <div id="answer" />
     </div>
